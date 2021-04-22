fb597ecbb89fbfca9791370bf3d0676b2248523c
yipin.wt <yipin.wt@cfuture.shop>
2021-04-22 16:51:25 +0800

    去掉MySQL相关 by yipin 2021-04-22

--- src/main/java/com/wt/test/rocketmq/RocketmqApplication.java
+++ src/main/java/com/wt/test/rocketmq/RocketmqApplication.java
@@ -3,7 +3,6 @@ package com.wt.test.rocketmq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RocketmqApplication {

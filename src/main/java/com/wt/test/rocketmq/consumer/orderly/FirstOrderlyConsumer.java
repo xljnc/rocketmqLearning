6563cf0b63c4f55bced4ff8b0e19e49ab1e112af
qiyu <jsj0804wt@126.com>
2023-04-25 11:19:30 +0800

    maven 转 gradle

--- src/main/java/com/wt/test/rocketmq/consumer/orderly/FirstOrderlyConsumer.java
+++ src/main/java/com/wt/test/rocketmq/consumer/orderly/FirstOrderlyConsumer.java
@@ -2,10 +2,12 @@ package com.wt.test.rocketmq.consumer.orderly;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.*;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerOrderly;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
-import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+import org.apache.rocketmq.remoting.protocol.heartbeat.MessageModel;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
